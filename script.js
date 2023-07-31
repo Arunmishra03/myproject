46e6606d5e0118e9c4f8b29e310b7b1015663c7e
Arunmishra03 <118375545+Arunmishra03@users.noreply.github.com>
2023-07-31 23:39:16 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = "036a6de1887c41d98eb05102232407";
-const aqikey="a676d23a-12af-46bd-b7b5-94197c396e0c";
+
 const form = document.querySelector("form");
 const search = document.querySelector("#location");
 
@@ -18,17 +18,22 @@ const search = document.querySelector("#location");
 //   console.log(lat,lon);
 // }
 
-const getWeather = async (url) => {
-  const response = await fetch(url);
+const getWeather = async (city) => {
+  const urlforecast = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7&aqi=yes&alerts=no`;
+ 
+  const response = await fetch(urlforecast);
   const data = await response.json();
   return data;
+  
+ 
 };
 function showWeather(weatherData) {
   console.log(weatherData);
   document.getElementById("temp_img").src = weatherData.current.condition.icon;
   document.getElementById("temp").innerHTML = weatherData.current.temp_c + "°C";
-  document.getElementById("realfeel").innerHTML = "Feels Like : "+weatherData.current.feelslike_c + "°C";
-  document.getElementById("uv").innerHTML = "UV : "+weatherData.current.uv ;
+  document.getElementById("realfeel").innerHTML =
+    "Feels Like : " + weatherData.current.feelslike_c + "°C";
+  document.getElementById("uv").innerHTML = "UV : " + weatherData.current.uv;
   document.getElementById("condition").innerHTML =
     weatherData.current.condition.text;
   document.getElementById("city").innerHTML =
@@ -73,9 +78,6 @@ function showWeather(weatherData) {
   const day = weatherData.forecast.forecastday;
   weeklyforecast(day);
   hourlyForecast(day);
- 
- 
-
 }
 function weeklyforecast(forecastday) {
   let d = new Date();
@@ -139,39 +141,35 @@ function weeklyforecast(forecastday) {
 }
 
 function hourlyForecast(forecastday) {
- let val= document.getElementById("localtime").innerText;
- if(val.slice(6)=="PM" && val.slice(0,2)!="12"){
-     const j= 12+parseInt(val.slice(0,2));
-    updateWeather(j+1,forecastday);
- }
-   else{
-    const j= parseInt(val.slice(0,2));
-    updateWeather(j+1,forecastday)
-
+  let val = document.getElementById("localtime").innerText;
+  if (val.slice(6) == "PM" && val.slice(0, 2) != "12") {
+    const j = 12 + parseInt(val.slice(0, 2));
+    updateWeather(j + 1, forecastday);
+  } else {
+    const j = parseInt(val.slice(0, 2));
+    updateWeather(j + 1, forecastday);
   }
-  
 }
-function updateWeather(j,forecastday){
-  let k=0;
-  for(let i=12;i<=24;i++ ){
-    if(j==24){
+function updateWeather(j, forecastday) {
+  let k = 0;
+  for (let i = 12; i <= 24; i++) {
+    if (j == 24) {
       k++;
-      j=j%24;
+      j = j % 24;
     }
-    document.getElementById(i).innerHTML=(forecastday[k].hour[j].time).slice(11);
-    document.getElementById((2*i)).src=forecastday[k].hour[j].condition.icon;
-    document.getElementById((4*i)).innerText=(forecastday[k].hour[j].temp_c) +"°C";
+    document.getElementById(i).innerHTML =
+      forecastday[k].hour[j].time.slice(11);
+    document.getElementById(2 * i).src = forecastday[k].hour[j].condition.icon;
+    document.getElementById(4 * i).innerText =
+      forecastday[k].hour[j].temp_c + "°C";
     j++;
-    //forecastday[k].hour[j].temp_c
-    // forecastday[k].hour[j].condition.icon
-    // forecastday[k].hour[j].time.slice(11);
   }
 }
 function localTime(timezone) {
   let d = new Date();
   const options = {
     timeZone: timezone,
-    hour12: true, 
+    hour12: true,
     hour: "2-digit",
     minute: "2-digit",
   };
@@ -184,44 +182,95 @@ function localTime(timezone) {
     localTimeString.slice(5, 9);
   document.getElementById("date").innerHTML = date;
 }
-function showAqi(current){
-  document.getElementById('pm2.5').innerHTML="PM2.5 : "+current.air_quality.pm2_5;
-  document.getElementById('pm10').innerHTML="PM10 : "+current.air_quality.pm10;
-  document.getElementById('co').innerHTML="CO : "+current.air_quality.co;
-  document.getElementById('no2').innerHTML="NO2 : "+current.air_quality.no2;
-  document.getElementById('o3').innerHTML="O3 : "+current.air_quality.o3;
-  document.getElementById('so2').innerHTML="SO2 : "+current.air_quality.so2;
-  const index=current.air_quality["gb-defra-index"];
+function showAqi(current) {
+  document.getElementById("pm2.5").innerHTML =
+    "PM2.5 : " + current.air_quality.pm2_5;
+  document.getElementById("pm10").innerHTML =
+    "PM10 : " + current.air_quality.pm10;
+  document.getElementById("co").innerHTML = "CO : " + current.air_quality.co;
+  document.getElementById("no2").innerHTML = "NO2 : " + current.air_quality.no2;
+  document.getElementById("o3").innerHTML = "O3 : " + current.air_quality.o3;
+  document.getElementById("so2").innerHTML = "SO2 : " + current.air_quality.so2;
+  const index = current.air_quality["gb-defra-index"];
   console.log(index);
-   ele=document.getElementById('index')
-   console.log(ele);
-  if(index>0 && index<=3){
-   ele.innerHTML="LOW";
-   ele.style.color="green";
+  ele = document.getElementById("index");
+  console.log(ele);
+  if (index > 0 && index <= 3) {
+    ele.innerHTML = "LOW";
+    ele.style.color = "green";
+  } else if (index >= 4 && index <= 6) {
+    ele.innerHTML = "Moderate";
+    ele.style.color = "#f6a200";
+  } else if (index >= 7 && index <= 9) {
+    ele.innerHTML = "High";
+    ele.style.color = "#ff3300";
+  } else {
+    ele.innerHTML = "High";
+    ele.style.color = "#730099";
   }
-  else if(index>=4 && index<=6){
-    ele.innerHTML="Moderate";
-   ele.style.color="#f6a200";
-  }
-  else if(index>=7 && index<=9){
-    ele.innerHTML="High";
-   ele.style.color="#ff3300";
-  }
-  else{
-    ele.innerHTML="High";
-   ele.style.color="#730099";
-  }
-
 }
 
 function currentWeather() {
-  
-  const urlforecast = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${search.value}&days=7&aqi=yes&alerts=no`;
- 
-  getWeather(urlforecast).then(showWeather);
- 
-
-
-  // getWeather(urlforecast)
-  // .then(showWeather)
+  getWeather(search.value).then(showWeather);
 }
+function weatherReport(){
+  getWeather(document.getElementById('city1').value)
+    .then(showWeatherReport);
+    getWeather(document.getElementById('city2').value)
+      .then(showWeatherReport);
+  
+}
+var count=8
+var id=10;
+function showWeatherReport(weatherData){
+   const data=weatherData;
+   console.log(data);
+   document.getElementById(id).innerHTML= `${weatherData.location.name} , ${weatherData.location.country}`;
+  if(id==10){
+    id++;
+  }
+  else{
+    id--;
+  }
+
+  for(let i=0;i<7;i++){
+    const data=weatherData.forecast.forecastday[i].day;
+   
+   const humidity=data.avghumidity+"%";
+   const temp=data.avgtemp_c+"°C";
+   const Wind=data.maxwind_kph+"Km/h";
+   const a=weatherData.forecast.forecastday[i].date;
+   const d=a.slice(8,)+a.slice(4,8)+a.slice(0,4);
+   const condition=data.condition.text;
+   const rain=data.daily_chance_of_rain+"%"
+    const row=document.createElement('tr');
+      const cell1 = document.createElement('td');
+      const cell2 = document.createElement('td');
+      const cell3 = document.createElement('td');
+      const cell4 = document.createElement('td');
+      const cell5 = document.createElement('td');
+      const cell6 = document.createElement('td');
+      cell1.appendChild(document.createTextNode(d));
+      cell2.appendChild(document.createTextNode(temp));
+      cell3.appendChild(document.createTextNode(condition));
+      cell4.appendChild(document.createTextNode(rain));
+      cell5.appendChild(document.createTextNode(humidity));
+      cell6.appendChild(document.createTextNode(Wind));
+      row.appendChild(cell1);
+      row.appendChild(cell2);
+      row.appendChild(cell3);
+      row.appendChild(cell4);
+      row.appendChild(cell5);
+      row.appendChild(cell6);
+      document.getElementById(count).appendChild(row);
+      
+
+    }
+    if(count==8){
+      count++;      
+    }
+    else{
+      count--;
+    }
+
+  }
